4006f90ed92fd8fa1e09faf0e2058db579cfb18a
jswan <jswannieman@gmail.com>
2023-05-03 10:22:20 +1000

    Commit ModalApp Javascript Component

---
+++ src/ModalApp.js
@@ -0,0 +1,50 @@
+import Modal from "./Modal";
+
+const ModalApp = () => {
+  return (
+    <>
+      <Modal>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+            optio cumque eveniet dolores? Similique, praesentium optio.
+            Assumenda accusantium, quis ab temporibus labore ipsa nisi odit,
+            iure iusto, esse in doloribus!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et est
+            facere expedita aspernatur tempore alias. Odio vitae nostrum libero
+            ad quisquam, porro veniam, fugit mollitia, excepturi maxime unde
+            alias eligendi!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, aut
+            aperiam. Officia amet eligendi, a neque velit nam voluptas nisi.
+            Maiores molestias vitae numquam beatae nesciunt iste, rem facilis
+            sequi!
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
+            ex quam enim nemo laboriosam sapiente inventore ipsa beatae? Culpa
+            eum impedit non in nihil laboriosam pariatur ut corrupti officia
+            nulla?
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+            nihil nobis dolores modi optio. Non velit iure sunt, explicabo vero,
+            necessitatibus voluptate tempore magnam vel vitae eveniet dolorem id
+            aliquid.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo natus
+            culpa expedita laboriosam quos itaque dolores commodi nemo mollitia
+            dicta fugiat molestias ex, consequatur eaque ratione cum! At,
+            corporis distinctio.
+          </li>
+        </ul>
+      </Modal>
+    </>
+  );
+};
+
+export default ModalApp;
